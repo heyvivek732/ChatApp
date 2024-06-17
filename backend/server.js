6c351eb6ce3585749765d68d87f0f0118e58ce5d
Vivek kumar <vivekkumar52522@gmail.com>
2024-06-17 19:03:48 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const {app,httpserver} =require('./socket/socket.js')
 require("dotenv").config();
 
+__dirname=path.resolve();
 app.use(bodyParser.json())
 app.use(cookieParser());
 app.use(express.json());
@@ -17,10 +18,16 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/messages', messageRoutes);
 app.use('/api/v1/users', userRouter);
 
+app.use(express.static(path.join(__dirname,"/frrontend/dist")))
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"frrontend","dist","index.html")); 
+})
+
 httpserver.listen(port, (req, res) => {
 
 })
 
 const connectdb = require('./Config/database');
 const { server } = require("./socket/socket");
+const { stat } = require("fs");
 connectdb();
